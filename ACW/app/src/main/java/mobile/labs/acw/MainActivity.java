eab70ab3ca483e38594e723eb03c51791bfed4a3
Aidan Fray <afray@hotmail.co.uk>
2018-02-17 13:10:46 +0000

    Changes; - Added buttons to the MainActivity - Added a scroll and table layout for the list of puzzles to download

--- ACW/app/src/main/java/mobile/labs/acw/MainActivity.java
+++ ACW/app/src/main/java/mobile/labs/acw/MainActivity.java
@@ -1,7 +1,10 @@
 package mobile.labs.acw;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Menu");
+    }
+
+    //Button_OnClick
+    public void puzzleDownload_Button_OnClick(View pView){
+        Log.i("MainActivity_LOG", "puzzleDownload_Button_OnClick() Clicked!");
+
+        Intent intent = new Intent(pView.getContext(), PuzzleDownload.class);
+        startActivity(intent);
+    }
+
+    public void highscore_Button_OnClick(View pView) {
+        Log.i("MainActivity_LOG", "highscore_Button_OnClick() Clicked!");
+
+        //TODO: Add intent code
     }
 }
