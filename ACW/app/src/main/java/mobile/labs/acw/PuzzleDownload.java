230663fe23d3cf8556a5039c01b694435f9a3514
Aidan Fray <afray@hotmail.co.uk>
2018-02-18 12:38:23 +0000

    Changes; - Puzzle index is now downloaded and added dynamically to the downloadable puzzle list

--- ACW/app/src/main/java/mobile/labs/acw/PuzzleDownload.java
+++ ACW/app/src/main/java/mobile/labs/acw/PuzzleDownload.java
@@ -1,37 +1,113 @@
 package mobile.labs.acw;
 
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import mobile.labs.acw.Views.PuzzleDownloadView;
 
 public class PuzzleDownload extends AppCompatActivity {
 
     LinearLayout mDownloadLayout;
+    Drawable mStockThumnnail;
+
+    final String mPuzzleIndexUrl = "index.json";
+    final String mPuzzleInfoUrl = "puzzles/";
+    final String mPuzzleLayoutUrl = "layouts/";
+    final String mPuzzleImageUrl = "images/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_download);
         setTitle("Puzzle Download");
-
         mDownloadLayout = (LinearLayout)findViewById(R.id.puzzle_download_layout);
+        mStockThumnnail = getResources().getDrawable(R.mipmap.ic_launcher);
 
-        //Test Examples
-        addDownloadPuzzle("Cat Puzzle 3x3");
-        addDownloadPuzzle("Computer Puzzle 10x10");
-        addDownloadPuzzle("Dog Puzzle 4x4");
+        //Downloads the JSON index for all the puzzles
+        new DownloadJSON().execute(mPuzzleIndexUrl);
     }
 
-    private void addDownloadPuzzle(String description) {
+    //Adds the custom control containing the puzzle code
+    private void addDownloadPuzzle(String pDescription, Drawable pThumbnail) {
         PuzzleDownloadView downloadRow = new PuzzleDownloadView(this);
-        downloadRow.setPuzzleDescription(description);
-
-        Drawable d = getResources().getDrawable(R.mipmap.ic_launcher);
-        downloadRow.setThumbnail(d);
-
+        downloadRow.setPuzzleDescription(pDescription);
+        downloadRow.setThumbnail(pThumbnail);
         mDownloadLayout.addView(downloadRow);
     }
+
+    private void addAllPuzzles(JSONObject jsonObject) {
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray("PuzzleIndex");
+
+            //Loops through each value and add to to the array
+            for (int i =0; i < jsonArray.length(); i++) {
+                String jsonString = jsonArray.get(i).toString();
+                addDownloadPuzzle(jsonString, mStockThumnnail);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class DownloadJSON extends AsyncTask<String, String, String> {
+
+        final String mBaseUrl = "http://www.simongrey.net/08027/slidingPuzzleAcw/";
+
+
+        private JSONObject mJSON;
+
+        @Override
+        protected String doInBackground(String... args) {
+
+            //Downloads JSON code from an url
+            InputStream inputStream = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                URL url = new URL(mBaseUrl + args[0]);
+                URLConnection connection = url.openConnection();
+                connection.setConnectTimeout(1000);
+                BufferedReader bufferedInputStream
+                        = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                String line = null;
+                while ((line = bufferedInputStream.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+
+                String result = stringBuilder.toString();
+                mJSON = new JSONObject(result);
+                bufferedInputStream.close();
+                return stringBuilder.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //Displays an error message
+            if (s == null) {
+                Toast.makeText(PuzzleDownload.this, "Error downloaing JSON puzzles", Toast.LENGTH_LONG).show();
+            }
+            else {
+                PuzzleDownload.this.addAllPuzzles(mJSON);
+            }
+        }
+    }
 }
+
--- ACW/app/src/main/java/mobile/labs/acw/Views/PuzzleDownloadView.java
+++ ACW/app/src/main/java/mobile/labs/acw/Views/PuzzleDownloadView.java
@@ -4,53 +4,99 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import mobile.labs.acw.R;
 
-public class PuzzleDownloadView extends LinearLayout {
+// Custom view that contains the value for the downloadable puzzles
+// It contains a Thumbnail, Description and Tick to show if it has been downloaded or not
+public class PuzzleDownloadView extends LinearLayout implements View.OnClickListener {
 
     private ImageView mThumbnail;
     private ImageView mDownloadStatus;
     private TextView mPuzzleDescription;
+    private Boolean mDownloadBool;
+    private ProgressBar mDownloadProgress;
 
-    private void Inflate(Context context) {
-        //Inflates the XML view
-        LayoutInflater inflater = LayoutInflater.from(context);
-        inflater.inflate(R.layout.puzzle_download_view, this);
-    }
+    private Drawable mDownloadStatus_Drawable;
 
     public PuzzleDownloadView(Context context) {
         super(context);
-        Inflate(context);
+        InflateNormalView(context);
         LoadViews();
     }
 
     public PuzzleDownloadView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        Inflate(context);
+        InflateNormalView(context);
         LoadViews();
     }
 
     private void LoadViews(){
+
+        //Grabs all controls
         mThumbnail = (ImageView)findViewById(R.id.puzzle_thumbnail_ImageView);
+        mThumbnail.setClickable(true);
+        mThumbnail.setOnClickListener(this);
         mDownloadStatus = (ImageView)findViewById(R.id.puzzle_download_status_ImageView);
+        mDownloadStatus.setImageDrawable(null);
+        mDownloadStatus.setClickable(true);
+        mDownloadStatus.setOnClickListener(this);
+        mDownloadStatus_Drawable = getResources().getDrawable(R.drawable.download_status);
         mPuzzleDescription = (TextView)findViewById(R.id.puzzle_description_TextView);
+        mPuzzleDescription.setClickable(true);
+        mPuzzleDescription.setOnClickListener(this);
+        mDownloadProgress = (ProgressBar)findViewById(R.id.puzzle_download_progess);
+        mDownloadBool = false;
+
     }
 
-    public void setThumbnail(Drawable icon) {
-        mThumbnail.setImageDrawable(icon);
+    private void InflateNormalView(Context context) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View normalView =  inflater.inflate(R.layout.puzzle_download_view, this, false);
+        this.removeAllViews();
+        this.addView(normalView);
     }
 
-    public void setPuzzleDescription(String text) {
-        mPuzzleDescription.setText(text);
+    private void InflateDownloadView(Context context) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View downloadView =  inflater.inflate(R.layout.puzzle_download_progress_view, this, false);
+        this.removeAllViews();
+        this.addView(downloadView);
     }
 
-    public void setmDownloadStatus(Boolean state) {
-        if (state) {
-            //TODO: Set tick image
+    @Override
+    public void onClick(View pView) {
+        //toggleDownloadStatus();
+        InflateDownloadView(getContext());
+    }
+
+    public void setThumbnail(Drawable pIcon) {
+        mThumbnail.setImageDrawable(pIcon);
+    }
+    public void setPuzzleDescription(String pDescription) {
+        mPuzzleDescription.setText(pDescription);
+    }
+    public void setDownloadStatus(Boolean pState) {
+        mDownloadBool = pState;
+        refreshDownloadStatusImage();
+    }
+    public void toggleDownloadStatus() {
+
+        //A toggle for the boolean value
+        mDownloadBool = !mDownloadBool;
+        refreshDownloadStatusImage();
+    }
+    private void refreshDownloadStatusImage() {
+        if (mDownloadBool) {
+            mDownloadStatus.setImageDrawable(mDownloadStatus_Drawable);
+        } else {
+            mDownloadStatus.setImageDrawable(null);
         }
     }
+
 }
