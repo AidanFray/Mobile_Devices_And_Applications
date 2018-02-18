190af6f7b238dcad5477e8dfdb8ac9b771c04d9a
Aidan Fray <afray@hotmail.co.uk>
2018-02-18 16:37:20 +0000

    Changes; - Loading bar is displayed while loading the puzzle index

--- ACW/app/src/main/java/mobile/labs/acw/PuzzleDownload.java
+++ ACW/app/src/main/java/mobile/labs/acw/PuzzleDownload.java
@@ -12,7 +12,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -22,6 +21,8 @@ import mobile.labs.acw.Views.PuzzleDownloadView;
 public class PuzzleDownload extends AppCompatActivity {
 
     LinearLayout mDownloadLayout;
+
+    //Temp placeholder
     Drawable mStockThumnnail;
 
     final String mPuzzleIndexUrl = "index.json";
@@ -34,50 +35,72 @@ public class PuzzleDownload extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_download);
         setTitle("Puzzle Download");
+
         mDownloadLayout = (LinearLayout)findViewById(R.id.puzzle_download_layout);
         mStockThumnnail = getResources().getDrawable(R.mipmap.ic_launcher);
 
         //Downloads the JSON index for all the puzzles
-        new DownloadJSON().execute(mPuzzleIndexUrl);
+        new Download_PuzzlePreviews().execute(mPuzzleIndexUrl);
     }
 
     //Adds the custom control containing the puzzle code
     private void addDownloadPuzzle(String pDescription, Drawable pThumbnail) {
+        //Adds each custom view
         PuzzleDownloadView downloadRow = new PuzzleDownloadView(this);
         downloadRow.setPuzzleDescription(pDescription);
         downloadRow.setThumbnail(pThumbnail);
         mDownloadLayout.addView(downloadRow);
     }
 
+    //Method that loops through all JSON values and adds them to the view
+    //This is also called when the AsyncTask has finished downloading
     private void addAllPuzzles(JSONObject jsonObject) {
+
+        //Hides the loading bar and spacing
+        mDownloadLayout.removeAllViews();
+
         try {
             JSONArray jsonArray = jsonObject.getJSONArray("PuzzleIndex");
 
             //Loops through each value and add to to the array
             for (int i =0; i < jsonArray.length(); i++) {
-                String jsonString = jsonArray.get(i).toString();
-                addDownloadPuzzle(jsonString, mStockThumnnail);
+
+                //Adds the puzzle with the JSON string
+                addDownloadPuzzle(jsonArray.get(i).toString(), mStockThumnnail);
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    private class DownloadJSON extends AsyncTask<String, String, String> {
+    //Downloads the index of different puzzles
+    private class Download_PuzzlePreviews extends AsyncTask<String, String, String> {
 
-        final String mBaseUrl = "http://www.simongrey.net/08027/slidingPuzzleAcw/";
-
-
+        private final String mBaseUrl = "http://www.simongrey.net/08027/slidingPuzzleAcw/";
         private JSONObject mJSON;
 
         @Override
         protected String doInBackground(String... args) {
+            Download_PuzzlePreview(args[0]);
+            return "";
+        }
 
+        @Override
+        protected void onPostExecute(String s) {
+            //Displays an error message
+            if (s == null) {
+                Toast.makeText(PuzzleDownload.this, "Error downloaing JSON puzzles", Toast.LENGTH_LONG).show();
+            }
+            else {
+                PuzzleDownload.this.addAllPuzzles(mJSON);
+            }
+        }
+
+        private void Download_PuzzlePreview(String s) {
             //Downloads JSON code from an url
-            InputStream inputStream = null;
             StringBuilder stringBuilder = new StringBuilder();
             try {
-                URL url = new URL(mBaseUrl + args[0]);
+                URL url = new URL(mBaseUrl + s);
                 URLConnection connection = url.openConnection();
                 connection.setConnectTimeout(1000);
                 BufferedReader bufferedInputStream
@@ -91,21 +114,8 @@ public class PuzzleDownload extends AppCompatActivity {
                 String result = stringBuilder.toString();
                 mJSON = new JSONObject(result);
                 bufferedInputStream.close();
-                return stringBuilder.toString();
             } catch (Exception e) {
                 e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            //Displays an error message
-            if (s == null) {
-                Toast.makeText(PuzzleDownload.this, "Error downloaing JSON puzzles", Toast.LENGTH_LONG).show();
-            }
-            else {
-                PuzzleDownload.this.addAllPuzzles(mJSON);
             }
         }
     }
